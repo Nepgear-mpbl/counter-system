45cb4d08c87bc235ca357ba8513edfb26315b0de
Rain <364687927@qq.com>
2017-11-27 09:35:58 +0800

    first commit

---
+++ counterSystem/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from config import config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+from .controllor import controller
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    print(config_name + ' mode on.')
+    db.init_app(app)
+    print('database initialized.')
+    app.register_blueprint(controller)
+    print('controller registered.')
+    print('application initialized.')
+    return app
---
+++ run.py
@@ -0,0 +1,4 @@
+from counterSystem import create_app
+
+if __name__ == '__main__':
+    create_app('development').run()
